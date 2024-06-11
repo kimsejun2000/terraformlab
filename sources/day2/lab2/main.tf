749005bdc997b5fb75e85fa69cc76c647df31dac
sejun.kim <sejun.kim@cloudmt.co.kr>
2024-06-12 05:48:45 +0900

    change day2 lab2

--- sources/day2/lab2/main.tf
+++ sources/day2/lab2/main.tf
@@ -24,3 +24,11 @@ module "instance2" {
     security_group_id = module.common.security_group_id
     tag_name = "sejun_day2_02"
 }
+
+output "instance1_public_ip" {
+    value = module.instance1.public_ip
+}
+
+output "instance2_public_ip" {
+    value = module.instance2.public_ip
+}
